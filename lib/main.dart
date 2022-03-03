f395f3ce6858d571b6c6a7dd8ee326ec4a7ea384
fjlealgs <fjlealgs@outlook.com>
2022-03-02 22:52:59 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: const Text('Eu sou rico'),
+          centerTitle: true,
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/rubi.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
